e12a456bb6b957eae8cd0bb0a0b14dd66c4eae10
Duarte Monteiro <du5rtemonteiro@gmail.com>
2024-07-11 23:57:02 +0100

    chore: tweak ignored tests

--- app/__tests__/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react-native';
-
-import Index from '../index';
-
-it('renders textual children', () => {
-  render(<Index />);
-  expect(screen.getByText('Products List')).toBeDefined();
-});
--- app/product/__tests__/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react-native';
-
-import ProductScreen from '../[id]';
-
-it('renders textual children', () => {
-  render(<ProductScreen />);
-  expect(screen.getByText('Product')).toBeDefined();
-});
